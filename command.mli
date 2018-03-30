80acbdca1791a7991ae252af95e39f68ce0683ae
Sahithi Kalvakota <sahithi.kalvakota@yahoo.com>
2018-03-29 23:09:15 -0400

    parse_click function

--- command.mli
+++ command.mli
@@ -6,5 +6,5 @@ type direction = X | Y
 (* [command] represents a command input by a player. *)
 type command = FLIP of direction | TURN of tile | PLACE of tile
 
-(*[parse_click ()]*)
+(*[parse_click ()] is the command selected by a mouse event*)
 val parse_click : unit -> command
