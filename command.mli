4c6b9cf83b6dc5e3bdcb7a1f9e0a45ac1f83edcf
Sahithi Kalvakota <sahithi.kalvakota@yahoo.com>
2018-03-29 22:40:23 -0400

    fixed command

--- command.mli
+++ command.mli
@@ -1,15 +1,7 @@
-(* [verb] represents all the possible verb inputs by a player. *)
-type verb = FLIP | TURN | PLACE
+Open Tile
+
+(*[direction] represents the axis the player wants flip a tile over*)
+type direction = X | Y
 
 (* [command] represents a command input by a player. *)
-type command = {verb: verb; obj: string}
-
-(* [parse str] is the command that represents player input [str].
- * requires: [str] is one of the following commands forms :
-        "flip [direction]" where direction is either "x" or "y"
-        "place [tile_name]" where tilename is a valid tile name
-          in the game
-        "turn [tile_name]" where tilename is a valid tile tile_name
-          in the game    
-*)
-val parse : string -> command
+type command = FLIP of direction | TURN of tile | PLACE of tile
