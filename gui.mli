b3a1b3f57621d1d8aaf28b04570fb7a1491119bd
Srishti Belwariar <sb2355@cornell.edu>
2018-03-29 21:41:52 -0400

    starting gui mli

---
+++ gui.mli
@@ -0,0 +1,35 @@
+open Lwt
+open Graphics
+open GMain
+open GObj
+
+(* [gui] represents the GUI window which will display State
+ * changes depending on user commands and acceptable actions.
+ * The GUI will determine the right state depending on actions
+ * taken by the player that are acceptable by the program.
+ * Reference: https://ocaml.org/learn/tutorials/objects.html
+ *)
+class gui:
+  (* [update_state] will update the GUI display with a new state.*)
+  method update_state: State.state -> unit
+
+  (* [get_command] will get the user's command or action.*)
+  method get_command: unit
+
+  (* [get_dim] returns the dimensions of the GUI window to prpoerly format
+   * the game board.*)
+  method get_dim: int * int
+
+(* [init_gui] initializes the GUI to the starting state *)
+val init_gui: gui ->unit
+
+(* [end_gui] ends the game.*)
+val end_gui: gui ->unit
+
+(* [draw_elts] will display the shapes and other elements
+ * needed in the GUI.*)
+val draw_elts: gui -> unit
+
+(* [get_dim] returns the dimensions of the GUI window to prpoerly format
+ * the game board.*)
+val get_window_dim: gui -> int * int
