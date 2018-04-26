5ec781b97b90c2a699e0b921a44f2cb95d96eef9
Sahithi Kalvakota <sahithi.kalvakota@yahoo.com>
2018-04-26 16:12:36 -0400

    implemented tile

--- tile.mli
+++ tile.mli
@@ -4,16 +4,20 @@ type tile_id =
   | One | Tee | L | X | Z | Tree | Line
   (* i love foster with all my heart <3 blokaml *)
 
-type color = |White |Blue |Yellow
+type color = White | Blue | Yellow
 
 (*[tile] is a type representing a tile in the game*)
 type tile = {
   name : tile_id;
   col : color;
+  value : int;
   grid: (int * int * color) array
 }
 
-(*[init_tile n coords c v] is the initial state of tile t*)
+(*[direction] represents the axis the player wants flip a tile over*)
+type direction = X | Y
+
+(*[init_tile id] is the initial state of tile t*)
 val init_tile : tile_id -> tile
 
 (*[tile_name t] is the name of tile t*)
@@ -25,8 +29,11 @@ val value : tile -> int
 (*[color t] is the color of tile t*)
 val color : tile -> color
 
+(*[color t] is the grid of tile t*)
+val grid: tile -> (int * int * color) array
+
 (*[flip_tile t dir] is tile t after a reflection over axis specified by [dir]*)
-val flip_tile : int*int list -> string -> int*int list
+val flip_tile : (int * int * color) array -> direction -> (int * int * color) array
 
 (*[turn_tile t] is tile t after a 90 degree turn clockwise*)
-val turn_tile : int*int list -> int*int list
+val turn_tile : (int * int * color) array  -> (int * int * color) array
