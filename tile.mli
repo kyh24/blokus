cf24653077e959f4f07503118951b08a6298f402
Devki Trivedi <devki98@gmail.com>
2018-04-27 23:19:51 -0400

    moved flip and turn over to state

--- tile.mli
+++ tile.mli
@@ -1,39 +1,73 @@
 
-type tile_id =
-  (* | One | Square | Xshape | Tee | Line | LilL | BigL *)
-  | One | Tee | L | X | Z | Tree | Line
-  (* i love foster with all my heart <3 blokaml *)
+type tile_id = One | Tee | L | X | Z | Tree | Line
 
 type color = White | Blue | Yellow
 
-(*[tile] is a type representing a tile in the game*)
 type tile = {
   name : tile_id;
   col : color;
   value : int;
-  mutable grid: (int * int * color) list
+  mutable grid: (int * int * color) list;
 }
 
-(*[direction] represents the axis the player wants flip a tile over*)
 type direction = X | Y
 
-(*[init_tile id] is the initial state of tile t*)
-val init_tile : tile_id -> color ->  tile
+let init_tile id c =
+  match id with
+  |One -> {name = One;
+           col = c;
+           value = 1;
+           grid = [(-1,1,White);  (0,1,White);  (1,1,White);
+                    (-1,0,White);  (0,0,c);      (1,0,White);
+                    (-1,-1,White); (0,-1,White); (1,-1,White);]
+          }
+  |Tee -> {name = Tee;
+           col= c;
+           value = 5;
+           grid = [(-1,1,c);      (0,1,c);  (1,1,c);
+                    (-1,0,White);  (0,0,c);  (1,0,White);
+                    (-1,-1,White); (0,-1,c); (1,-1,White);]
+          }
+  |L -> {name = L;
+         col = c;
+         value = 5;
+         grid = [(-1,1,c);  (0,1,White);  (1,1,White);
+                  (-1,0,c);  (0,0,White);  (1,0,White);
+                  (-1,-1,c); (0,-1,White); (1,-1,c);]
+        }
+  |X -> {name = X;
+         col = c;
+         value = 5;
+         grid = [(-1,1,White);  (0,1,c);  (1,1,White);
+                  (-1,0,c);      (0,0,c);  (1,0,c);
+                  (-1,-1,White); (0,-1,c); (1,-1,White);]
+        }
+  |Z -> {name = Z;
+         col = c;
+         value = 5;
+         grid = [(-1,1,c);      (0,1,c);  (1,1,White);
+                  (-1,0,White);  (0,0,c);  (1,0,White);
+                  (-1,-1,White); (0,-1,c); (1,-1,c);]
+        }
+  |Tree -> {name = Tree;
+            col = c;
+            value = 5;
+            grid = [(-1,1,c);      (0,1,c);  (1,1,White);
+                     (-1,0,White);  (0,0,c);  (1,0,c);
+                     (-1,-1,White); (0,-1,c); (1,-1,White);]
+           }
+  |Line -> {name = Line;
+            col = c;
+            value = 3;
+            grid = [(-1,1,White);  (0,1,c);  (1,1,White);
+                     (-1,0,White);  (0,0,c);  (1,0,White);
+                     (-1,-1,White); (0,-1,c); (1,-1,White);]
+           }
 
-(*[tile_name t] is the name of tile t*)
-val tile_name : tile -> tile_id
+let tile_name t = t.name
 
-(*[value t] is the value of tile t*)
-val value : tile -> int
+let value t = t.value
 
-(*[color t] is the color of tile t*)
-val color : tile -> color
+let color t = t.col
 
-(*[color t] is the grid of tile t*)
-val grid: tile -> (int * int * color) list
-
-(*[flip_tile t dir] is tile t after a reflection over axis specified by [dir]*)
-val flip_tile : tile -> direction -> unit
-
-(*[turn_tile t] is tile t after a 90 degree turn clockwise*)
-val turn_tile: tile -> unit
+let grid t = t.grid
