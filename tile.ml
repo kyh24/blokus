5f82d0f7f29003bb873dfaa4c2cb1940cee564d4
kyh24 <34723760+kyh24@users.noreply.github.com>
2018-04-28 18:43:19 -0400

    trying to work out game logic in state.ml

--- tile.ml
+++ tile.ml
@@ -7,7 +7,8 @@ type tile = {
   name : tile_id;
   col : color;
   value : int;
-  mutable grid: (int * int * color) list;
+  mutable grid: ((int * int) * color) list;
+  mutable corners: (int *int) list;
 }
 
 type direction = X | Y
@@ -17,51 +18,58 @@ let init_tile id c =
   |One -> {name = One;
            col = c;
            value = 1;
-           grid = [(-1,1,White);  (0,1,White);  (1,1,White);
-                    (-1,0,White);  (0,0,c);      (1,0,White);
-                    (-1,-1,White); (0,-1,White); (1,-1,White);]
+           grid = [((-1,1),White);  ((0,1),White);  ((1,1),White);
+                   ((-1,0),White);  ((0,0),c);      ((1,0),White);
+                   ((-1,-1),White); ((0,-1),White); ((1,-1),White)];
+           corners = [(-1,1);(1,1);(-1,-1);(1,-1)]
           }
   |Tee -> {name = Tee;
            col= c;
            value = 5;
-           grid = [(-1,1,c);      (0,1,c);  (1,1,c);
-                    (-1,0,White);  (0,0,c);  (1,0,White);
-                    (-1,-1,White); (0,-1,c); (1,-1,White);]
+           grid = [((-1,1),c);      ((0,1),c);  ((1,1),c);
+                   ((-1,0),White);  ((0,0),c);  ((1,0),White);
+                   ((-1,-1),White); ((0,-1),c); ((1,-1),White)];
+           corners = [(-2,2);(2,2);(-1,-2);(1,-2)]
           }
   |L -> {name = L;
          col = c;
          value = 5;
-         grid = [(-1,1,c);  (0,1,White);  (1,1,White);
-                  (-1,0,c);  (0,0,White);  (1,0,White);
-                  (-1,-1,c); (0,-1,c); (1,-1,c);]
+         grid = [((-1,1),c);  ((0,1),White);  ((1,1),White);
+                 ((-1,0),c);  ((0,0),White);      ((1,0),White);
+                 ((-1,-1),c); ((0,-1),c); ((1,-1),c)];
+         corners = [(-2,2);(0,2);(-2,-2);(2,-2);(2,0)]
         }
   |X -> {name = X;
          col = c;
          value = 5;
-         grid = [(-1,1,White);  (0,1,c);  (1,1,White);
-                  (-1,0,c);      (0,0,c);  (1,0,c);
-                  (-1,-1,White); (0,-1,c); (1,-1,White);]
+         grid = [((-1,1),White);  ((0,1),c);  ((1,1),White);
+                 ((-1,0),c);  ((0,0),c);      ((1,0),c);
+                 ((-1,-1),White); ((0,-1),c); ((1,-1),White)];
+         corners = [(-1,2);(1,2);(2,1);(2,-1);(-2,1);(-2,-1);(-1,-2);(1,-2)]
         }
   |Z -> {name = Z;
          col = c;
          value = 5;
-         grid = [(-1,1,c);      (0,1,c);  (1,1,White);
-                  (-1,0,White);  (0,0,c);  (1,0,White);
-                  (-1,-1,White); (0,-1,c); (1,-1,c);]
+         grid = [((-1,1),c);  ((0,1),c);  ((1,1),White);
+                 ((-1,0),White);  ((0,0),c);      ((1,0),White);
+                 ((-1,-1),White); ((0,-1),c); ((1,-1),c)];
+         corners = [(-2,2);(-2,0);(1,1);(-1,-2);(2,0);(2,-2)]
         }
   |Tree -> {name = Tree;
             col = c;
             value = 5;
-            grid = [(-1,1,c);      (0,1,c);  (1,1,White);
-                     (-1,0,White);  (0,0,c);  (1,0,c);
-                     (-1,-1,White); (0,-1,c); (1,-1,White);]
+            grid = [((-1,1),c);  ((0,1),c);  ((1,1),White);
+                    ((-1,0),White);  ((0,0),c);      ((1,0),c);
+                    ((-1,-1),White); ((0,-1),c); ((1,-1),White)];
+            corners = [(-2,2);(-2,0);(1,2);(2,1);(2,-1);(1,-2);(-1,-2)]
            }
   |Line -> {name = Line;
             col = c;
             value = 3;
-            grid = [(-1,1,White);  (0,1,c);  (1,1,White);
-                     (-1,0,White);  (0,0,c);  (1,0,White);
-                     (-1,-1,White); (0,-1,c); (1,-1,White);]
+            grid = [((-1,1),White);  ((0,1),c);  ((1,1),White);
+                    ((-1,0),White);  ((0,0),c);      ((1,0),White);
+                    ((-1,-1),White); ((0,-1),c); ((1,-1),White)];
+            corners = [(-1,2);(1,1);(1,-1);(-1,-1)]
            }
 
 let tile_name t = t.name
@@ -71,3 +79,31 @@ let value t = t.value
 let color t = t.col
 
 let grid t = t.grid
+
+let flip_tile t dir =
+  let new_grid = List.map (fun ((x,y),_) ->
+      let coord =
+        begin
+          match dir with
+          |X -> List.find (fun ((a,b),_) -> a=x && b=(-y)) t.grid
+          |Y -> List.find (fun ((a,b),_) -> a=(-x) && b=y) t.grid
+        end in
+      let new_c = match coord with ((_,_),c) -> c in
+      ((x,y),new_c)) t.grid in
+  t.grid <- new_grid;
+  t.corners <-
+    begin
+      match dir with
+      |X-> List.map(fun (x,y) -> (x,-y)) t.corners
+      |Y -> List.map(fun (x,y) -> (-x,y)) t.corners
+    end;
+  t
+
+let turn_tile t =
+  let new_grid = List.map (fun ((x,y),_) ->
+      let coord = List.find (fun ((a,b),_) -> a= (-y) && b=x) t.grid in
+      let new_c = match coord with ((_,_),c) -> c in
+      ((x,y),new_c)) t.grid in
+  t.grid <- new_grid;
+  t.corners <- List.map(fun (x,y) -> (-y,x)) t.corners;
+  t
