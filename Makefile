9989fac729da21bfb6cb9fa16ff440e61867a683
Srishti Belwariar <sb2355@cornell.edu>
2018-04-28 14:34:36 -0400

    gui

--- Makefile
+++ Makefile
@@ -5,6 +5,4 @@ clean:
 	ocamlbuild -clean
 
 play:
-	ocamlbuild -use-ocamlfind -pkg graphics main.byte && ./main.byte
-	# ocamlbuild -use-ocamlfind main.byte && ./main.byte
-# ocamlbuild -pkgs oUnit,yojson,str,graphics -tag thread main.byte && ./main.byte
+	ocamlbuild -pkgs unix,oUnit,graphics -tag thread main.byte && ./main.byte -gui
--- main.ml
+++ main.ml
@@ -1,4 +1,20 @@
 open Graphics
+open Printf
+
+
+let rec loop () =
+  let e = wait_next_event [Mouse_motion; Key_pressed] in
+
+  let mouse_description = sprintf "Mouse position: %d,%d" e.mouse_x e.mouse_y in
+
+
+  clear_graph ();
+  moveto 0 0; draw_string mouse_description;
+
+  if e.key <> 'q' then loop () else ()
 
 let () =
-  open_graph " 1000x750";
+  open_graph " 700x700";
+  Graphics.set_window_title "bLoKaML by Srishti B., Kati H., Sahithi K., & Devki T.";
+  loop ();
+  close_graph ();
