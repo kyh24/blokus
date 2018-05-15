968070880b9a5a4dbf85ef018a2a16d70195fb09
Srishti Belwariar <sb2355@cornell.edu>
2018-05-15 14:15:54 -0400

    update command.mli

--- command.mli
+++ command.mli
@@ -1,7 +1,4 @@
 open Tile
 
-(*[direction] represents the axis the player wants flip a tile over*)
-type direction = X | Y
-
 (* [command] represents a command input by a player. *)
 type command = FLIPX of tile_id | FLIPY of tile_id | TURN of tile_id | PLACE of (int * int) * tile_id | FORFEIT
