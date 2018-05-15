2fbcd6c24e0248b2ee63cf5a0c95ce75c710be73
Sahithi Kalvakota <sk2679@cornell.edu>
2018-05-14 21:38:41 -0400

    fixed up specs

---
+++ board.mli
@@ -0,0 +1,15 @@
+open Tile
+
+type board= ((int * int) * color) array
+
+(*[init_board brd_size] initializes a board with dimensions brd_size x brd_size*)
+val init_board: int -> ((int * int) * color) array
+
+(*[brd_size brd] returns the height of board [brd]*)
+val brd_size : board -> int
+
+(*[get_index (x,y) size] returns the array index of coordinate [(x,y)] *)
+val get_index : int*int -> int -> int
+
+(*[place_tile_on_brd tile_colors brd] places the tile on [tile_colors] onto [brd] ]*)
+val place_tile_on_brd : ((int * int) * color) -> board -> board
