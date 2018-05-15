6ec53aa8dd098699fdb75a0de675d5a872d8a869
vagrant <vagrant@vm3110-2018sp>
2018-05-15 13:33:57 -0400

    104 tests written

--- test.ml
+++ test.ml
@@ -2,11 +2,12 @@ open OUnit2
 open State
 open Tile
 open Player
+open List
 
 let rec compare_lsts lst1 lst2 =
   match lst1 with
   | [] -> true
-  | h::t -> List.mem h lst2 && compare_lsts t lst2
+  | h::t -> mem h lst2 && compare_lsts t lst2
 
 let t1 = init_tile One Blue
 let grid_t1 = [((-1,1),White);  ((0,1),White);  ((1,1),White);
@@ -90,14 +91,63 @@ let player_1 = init_player "Player 1" Yellow
 let player_2 = init_player "Player 2" Blue
 let t1_init = init_tile One Yellow
 let t1_init2 = init_tile One Yellow
+let t1_init3 = init_tile One Yellow
+let t1_init4 = init_tile Tee Yellow
+let t1_init5 = init_tile Cowgirl Yellow
+let p1_m2 = init_tile Cowgirl Yellow
+let p2_m2 = init_tile Cowgirl Blue
+let t1_init6 = init_tile One Yellow
+let t1_init7 = init_tile One Yellow
+let t1_init8 = init_tile One Yellow
 let t2_init = init_tile One Blue
+let t2_init6 = init_tile One Blue
+let t2_init7 = init_tile One Blue
+let t2_init8 = init_tile One Blue
+let p1_m2_8 = init_tile Cowgirl Yellow
+let p2_m2_8 = init_tile Cowgirl Blue
 let t1' = init_tile Tee Yellow
 
-(*st2*)
+(*st2 - p1 first move*)
 let st2 = do_command (PLACE ((404,570),t1_init.name)) init2'
 let st2_invalid_move = do_command (PLACE ((780,176),t1_init2.name)) init3'
 
-(*st3*)
+(*st3 - p2 first move*)
+let init4 = 16 |> init_state |> do_command (PLACE ((404,570),t1_init.name))
+let st3 =  init4 |> do_command (PLACE ((780,176),t2_init.name))
+
+(*st4 - turn canvas*)
+let init4 = 16 |> init_state
+let init4' = {init4 with canvas1 = t1_init4.grid} |> do_command (TURN t1_init4.name)
+
+(*st5 - flip canvas*)
+let init5 = 16 |> init_state
+let init5' = {init5 with canvas1 = t1_init5.grid} |> do_command (FLIPX t1_init5.name)
+
+(*st6- p1 second move = play mode*)
+let init6 = 16 |> init_state
+let init6' = {init6 with canvas1 = t1_init6.grid} |> do_command (PLACE ((404,570),t1_init.name))
+let init6'' = {init6' with canvas2 = t2_init6.grid} |> do_command (PLACE ((780,176),t2_init6.name))
+let init6''' = {init6'' with canvas1 = p1_m2.grid} |> do_command (PLACE ((426,546),p1_m2.name))
+
+(*st7- p2 second move = invalid*)
+let init7 = 16 |> init_state
+let init7' = {init7 with canvas1 = t1_init7.grid} |> do_command (PLACE ((404,570),t1_init7.name))
+let init7'' = {init7' with canvas2 = t2_init7.grid} |> do_command (PLACE ((780,176),t2_init7.name))
+let init7_3 = {init7'' with canvas1 = p1_m2.grid} |> do_command (PLACE ((426,546),p1_m2.name))
+let init7_p2 = {init7_3 with canvas2 = p2_m2.grid} |> do_command (PLACE ((600,546),p2_m2.name))
+
+(*st8- p2 second move = valid*)
+let init8 = 16 |> init_state
+let init8' = {init8 with canvas1 = t1_init8.grid} |> do_command (PLACE ((404,570),t1_init8.name))
+let init8'' = {init8' with canvas2 = t2_init8.grid} |> do_command (PLACE ((780,176),t2_init8.name))
+let init8_3 = {init8'' with canvas1 = p1_m2.grid} |> do_command (PLACE ((426,546),p1_m2.name))
+let init8_p2 = {init8_3 with canvas2 = p2_m2.grid} |> do_command (PLACE ((746,226),p2_m2.name))
+
+(*st9 - both = forfeit*)
+let init9 = 16 |> init_state
+let init9' = {init9 with canvas1 = t1_init6.grid} |> do_command (PLACE ((404,570),t1_init.name))
+let init9'' = {init9' with canvas2 = t2_init6.grid} |> do_command (PLACE ((780,176),t2_init6.name))
+let init9_p1_f = (init9'' |> do_command FORFEIT) |> do_command FORFEIT
 
 (*updating players*)
 let p1_invalid_move1 = {player_name = "Player 1";
@@ -111,17 +161,41 @@ let p1_invalid_move1 = {player_name = "Player 1";
                                            init_tile Four Yellow; init_tile Boot Yellow;
                                            init_tile Kink Yellow; init_tile C Yellow;
                                            init_tile Cowgirl Yellow; init_tile Couch Yellow;
-                                           init_tile Recliner Yellow;init_tile Stairmaster Yellow];
-                       }
-let player_1_3 = {player_name = "Player 1";
-                  col = Yellow;
-                  score = 1;
-                  status = Play;
-                  remaining_tiles = [init_tile Tee Yellow;
-                                     init_tile L Yellow; init_tile X Yellow;
-                                     init_tile Z Yellow; init_tile Tree Yellow;
-                                     init_tile Line Yellow];
-                 }
+                                           init_tile Recliner Yellow;init_tile Stairmaster Yellow];}
+
+let p1_turn = {player_name = "Player 1";
+                        col = Yellow;
+                        score = 0;
+                        status = Start;
+                        remaining_tiles = [init_tile One Yellow; t1_init4;
+                                           init_tile L Yellow; init_tile X Yellow;
+                                           init_tile Z Yellow; init_tile Tree Yellow;
+                                           init_tile Line Yellow; init_tile Two Yellow;
+                                           init_tile Four Yellow; init_tile Boot Yellow;
+                                           init_tile Kink Yellow; init_tile C Yellow;
+                                           init_tile Cowgirl Yellow; init_tile Couch Yellow;
+                                           init_tile Recliner Yellow;init_tile Stairmaster Yellow];}
+
+let p1_move_1 = {player_1 with score = 1; status = Play;
+                               remaining_tiles = [init_tile Tee Yellow;
+                                                  init_tile L Yellow; init_tile X Yellow;
+                                                  init_tile Z Yellow; init_tile Tree Yellow;
+                                                  init_tile Line Yellow; init_tile Two Yellow;
+                                                  init_tile Four Yellow; init_tile Boot Yellow;
+                                                  init_tile Kink Yellow; init_tile C Yellow;
+                                                  init_tile Cowgirl Yellow; init_tile Couch Yellow;
+                                                  init_tile Recliner Yellow;init_tile Stairmaster Yellow]}
+let p2_move_1 = {player_2 with score = 1; status = Play;
+                 remaining_tiles = [init_tile Tee Yellow;
+                  init_tile L Yellow; init_tile X Yellow;
+                  init_tile Z Yellow; init_tile Tree Yellow;
+                  init_tile Line Yellow; init_tile Two Yellow;
+                  init_tile Four Yellow; init_tile Boot Yellow;
+                  init_tile Kink Yellow; init_tile C Yellow;
+                  init_tile Cowgirl Yellow; init_tile Couch Yellow;
+                  init_tile Recliner Yellow;init_tile Stairmaster Yellow]}
+
+
 (********************************************************)
 (*************************TILE ROTATION AND FLIP TESTS***************************)
 let tests = [
@@ -213,7 +287,7 @@ let state_tests = [
                           ((-1,-1),White); ((0,-1),White); ((1,-1),White)] init.canvas2);
 
   "init_player_stat" >:: (fun _ -> assert_equal Start (init.curr_player).status);
-  "init_player_stat2">:: (fun _ -> assert_equal Start (List.nth init.players 1).status);
+  "init_player_stat2">:: (fun _ -> assert_equal Start (nth init.players 1).status);
   "init_curr_p" >:: (fun _ -> assert_equal player_1 (init.curr_player));
 
   (*Player 1 valid first move*)
@@ -226,11 +300,11 @@ let state_tests = [
                                             ((-1,0),White);  ((0,0),White);  ((1,0),White);
                                             ((-1,-1),White); ((0,-1),White); ((1,-1),White)]
                         (st2.canvas2));
-  "st2_p1_stat" >:: (fun _ -> assert_equal Play (List.nth st2.players 0).status);
-  "st2_p2_stat" >:: (fun _ -> assert_equal Start (List.nth st2.players 1).status);
+  "st2_p1_stat" >:: (fun _ -> assert_equal Play (nth st2.players 0).status);
+  "st2_p2_stat" >:: (fun _ -> assert_equal Start (nth st2.players 1).status);
   "st2_board" >:: (fun _ -> assert_equal (Array.get (st2.board) 0) ((0,0),Yellow));
-  "st2_p1_score" >:: (fun _ -> assert_equal 1 (List.nth st2.players 0).score);
-  "st2_p2_score" >:: (fun _ -> assert_equal 0 (List.nth st2.players 1).score);
+  "st2_p1_score" >:: (fun _ -> assert_equal 1 (nth st2.players 0).score);
+  "st2_p2_score" >:: (fun _ -> assert_equal 0 (nth st2.players 1).score);
 
   (*Player 1 invalid first move*)
   "st2'_curr_p" >:: (fun _ -> assert_equal p1_invalid_move1 (st2_invalid_move.curr_player));
@@ -242,15 +316,118 @@ let state_tests = [
                                              ((-1,0),White);  ((0,0),White);  ((1,0),White);
                                              ((-1,-1),White); ((0,-1),White); ((1,-1),White)]
                          (st2_invalid_move.canvas2));
-  "st2'_p1_stat" >:: (fun _ -> assert_equal Start (List.nth st2_invalid_move.players 0).status);
-  "st2'_p2_stat" >:: (fun _ -> assert_equal Start (List.nth st2.players 1).status);
-  "st2'_p1_score" >:: (fun _ -> assert_equal 0 (List.nth st2_invalid_move.players 0).score);
-  "st2'_p2_score" >:: (fun _ -> assert_equal 0 (List.nth st2_invalid_move.players 1).score);
+  "st2'_p1_stat" >:: (fun _ -> assert_equal Start (nth st2_invalid_move.players 0).status);
+  "st2'_p2_stat" >:: (fun _ -> assert_equal Start (nth st2_invalid_move.players 1).status);
+  "st2'_p1_score" >:: (fun _ -> assert_equal 0 (nth st2_invalid_move.players 0).score);
+  "st2'_p2_score" >:: (fun _ -> assert_equal 0 (nth st2_invalid_move.players 1).score);
   "st2'_board" >:: (fun _ -> assert_equal (Array.get (st2_invalid_move.board) 255) ((15,15),White));
 
   (*Player 2 first move*)
+  "st3_curr_p" >:: (fun _ -> assert_equal p1_move_1 (st3.curr_player));
+  "st3_canvas1" >:: (fun _ -> assert_equal [((-1,1),White);  ((0,1),White);  ((1,1),White);
+                                             ((-1,0),White);  ((0,0),White);  ((1,0),White);
+                                             ((-1,-1),White); ((0,-1),White); ((1,-1),White)]
+                        (st3.canvas1));
+  "st3_canvas2" >:: (fun _ -> assert_equal [((-1,1),White);  ((0,1),White);  ((1,1),White);
+                                             ((-1,0),White);  ((0,0),White);  ((1,0),White);
+                                             ((-1,-1),White); ((0,-1),White); ((1,-1),White)]
+                         (st3.canvas2));
+  "st3_p1_stat" >:: (fun _ -> assert_equal Play (nth st3.players 0).status);
+  "st3_p2_stat" >:: (fun _ -> assert_equal Play (nth st3.players 1).status);
+  "st3_p1_score" >:: (fun _ -> assert_equal 1 (nth st3.players 0).score);
+  "st3_p2_score" >:: (fun _ -> assert_equal 1 (nth st3.players 1).score);
+  "st3_board" >:: (fun _ -> assert_equal (Array.get (st3.board) 255) ((15,15),Blue));
+
+  (*Turn canvas command*)
+  "st4_canvas1" >:: (fun _ -> assert_equal [((-1,1),White);  ((0,1),White);  ((1,1),Yellow);
+                                             ((-1,0),Yellow);  ((0,0),Yellow);  ((1,0),Yellow);
+                                             ((-1,-1),White); ((0,-1),White); ((1,-1),Yellow)]
+                        (init4'.canvas1));
+  "st4_canvas2" >:: (fun _ -> assert_equal [((-1,1),White);  ((0,1),White);  ((1,1),White);
+                                             ((-1,0),White);  ((0,0),White);  ((1,0),White);
+                                             ((-1,-1),White); ((0,-1),White); ((1,-1),White)]
+                         (init4'.canvas2));
+  "st4_p1_stat" >:: (fun _ -> assert_equal Start (nth init4'.players 0).status);
+  "st4_p2_stat" >:: (fun _ -> assert_equal Start (nth init4'.players 1).status);
+  "st4_p1_score" >:: (fun _ -> assert_equal 0 (nth init4'.players 0).score);
+  "st4_p2_score" >:: (fun _ -> assert_equal 0 (nth init4'.players 1).score);
+
+  (*Flip canvas command*)
+  "st5_canvas1" >:: (fun _ -> assert_equal [((-1,1),Yellow);  ((0,1),Yellow);  ((1,1),Yellow);
+                                             ((-1,0),White);  ((0,0),Yellow);  ((1,0),White);
+                                             ((-1,-1),White); ((0,-1),White); ((1,-1),White)]
+                        (init5'.canvas1));
+  "st5_canvas2" >:: (fun _ -> assert_equal [((-1,1),White);  ((0,1),White);  ((1,1),White);
+                                             ((-1,0),White);  ((0,0),White);  ((1,0),White);
+                                             ((-1,-1),White); ((0,-1),White); ((1,-1),White)]
+                         (init5'.canvas2));
+  "st5_p1_stat" >:: (fun _ -> assert_equal Start (nth init5'.players 0).status);
+  "st5_p2_stat" >:: (fun _ -> assert_equal Start (nth init5'.players 1).status);
+  "st5_p1_score" >:: (fun _ -> assert_equal 0 (nth init5'.players 0).score);
+  "st5_p2_score" >:: (fun _ -> assert_equal 0 (nth init5'.players 1).score);
+
+  (*Player 1 second move*)
+  "st6_canvas1" >:: (fun _ -> assert_equal [((-1,1),White);  ((0,1),White);  ((1,1),White);
+                                             ((-1,0),White);  ((0,0),White);  ((1,0),White);
+                                             ((-1,-1),White); ((0,-1),White); ((1,-1),White)]
+                        (init6'''.canvas1));
+  "st3_canvas2" >:: (fun _ -> assert_equal [((-1,1),White);  ((0,1),White);  ((1,1),White);
+                                             ((-1,0),White);  ((0,0),White);  ((1,0),White);
+                                             ((-1,-1),White); ((0,-1),White); ((1,-1),White)]
+                         (init6'''.canvas2));
+  "st3_p1_stat" >:: (fun _ -> assert_equal Play (nth init6'''.players 0).status);
+  "st3_p2_stat" >:: (fun _ -> assert_equal Play (nth init6'''.players 1).status);
+  "st3_p1_score" >:: (fun _ -> assert_equal 5 (nth init6'''.players 0).score);
+  "st3_p2_score" >:: (fun _ -> assert_equal 1 (nth init6'''.players 1).score);
+  "st3_board" >:: (fun _ -> assert_equal (Array.get (init6'''.board) 17) ((1,1),Yellow));
+
+  (*Player 2 second move - invalid*)
+  "st7_canvas1" >:: (fun _ -> assert_equal [((-1,1),White);  ((0,1),White);  ((1,1),White);
+                                             ((-1,0),White);  ((0,0),White);  ((1,0),White);
+                                             ((-1,-1),White); ((0,-1),White); ((1,-1),White)]
+                        (init7_p2.canvas1));
+  "st7_canvas2" >:: (fun _ -> assert_equal [((-1,1),White);  ((0,1),White);  ((1,1),White);
+                                             ((-1,0),White);  ((0,0),Blue);  ((1,0),White);
+                                             ((-1,-1),Blue); ((0,-1),Blue); ((1,-1),Blue)]
+                         (init7_p2.canvas2));
+  "st7_p1_stat" >:: (fun _ -> assert_equal Play (nth init7_p2.players 0).status);
+  "st7_p2_stat" >:: (fun _ -> assert_equal Play (nth init7_p2.players 1).status);
+  "st7_p1_score" >:: (fun _ -> assert_equal 5 (nth init7_p2.players 0).score);
+  "st7_p2_score" >:: (fun _ -> assert_equal 1 (nth init7_p2.players 1).score);
+  "st7_status" >:: (fun _ -> assert_equal false init7_p2.game_over);
+
+  (*Player 2 second move - valid*)
+  "st8_canvas1" >:: (fun _ -> assert_equal [((-1,1),White);  ((0,1),White);  ((1,1),White);
+                                             ((-1,0),White);  ((0,0),White);  ((1,0),White);
+                                             ((-1,-1),White); ((0,-1),White); ((1,-1),White)]
+                        (init8_p2.canvas1));
+  "st8_canvas2" >:: (fun _ -> assert_equal [((-1,1),White);  ((0,1),White);  ((1,1),White);
+                                            ((-1,0),White);  ((0,0),White);  ((1,0),White);
+                                            ((-1,-1),White); ((0,-1),White); ((1,-1),White)]
+                         (init8_p2.canvas2));
+  "st8_p1_stat" >:: (fun _ -> assert_equal Play (nth init8_p2.players 0).status);
+  "st8_p2_stat" >:: (fun _ -> assert_equal Play (nth init8_p2.players 1).status);
+  "st8_p1_score" >:: (fun _ -> assert_equal 5 (nth init8_p2.players 0).score);
+  "st8_p2_score" >:: (fun _ -> assert_equal 5 (nth init8_p2.players 1).score);
+  "st8_status" >:: (fun _ -> assert_equal false init8_p2.game_over);
+
+  (*FORFEIT*)
+  "st9_canvas1" >:: (fun _ -> assert_equal [((-1,1),White);  ((0,1),White);  ((1,1),White);
+                                            ((-1,0),White);  ((0,0),White);  ((1,0),White);
+                                            ((-1,-1),White); ((0,-1),White); ((1,-1),White)]
+                        (init9_p1_f.canvas1));
+  "st9_canvas2" >:: (fun _ -> assert_equal [((-1,1),White);  ((0,1),White);  ((1,1),White);
+                                            ((-1,0),White);  ((0,0),White);  ((1,0),White);
+                                            ((-1,-1),White); ((0,-1),White); ((1,-1),White)]
+                        (init9_p1_f.canvas2));
+  "st9_p1_stat" >:: (fun _ -> assert_equal Stop (nth init9_p1_f.players 0).status);
+  "st9_p2_stat" >:: (fun _ -> assert_equal Stop (nth init9_p1_f.players 1).status);
+  "st9_p1_score" >:: (fun _ -> assert_equal 1 (nth init9_p1_f.players 0).score);
+  "st9_p2_score" >:: (fun _ -> assert_equal 1 (nth init9_p1_f.players 1).score);
+  "st9_status" >:: (fun _ -> assert_equal true init9_p1_f.game_over);
+
 
 ]
 
-let suite = "State Test Suite" >::: List.flatten [tests;state_tests]
+let suite = "State Test Suite" >::: flatten [tests;state_tests]
 let _ = run_test_tt_main suite
